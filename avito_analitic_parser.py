478f7f3543052763d4f332028cc5bfb2cde526dd
colosss <kocnevs.jk@yandex.ru>
2026-02-27 17:56:46 +0300

    Add analitic data

--- avito_analitic_parser.py
+++ avito_analitic_parser.py
@@ -6,12 +6,85 @@ from time import sleep
 import pandas as pd
 import subprocess
 
-PAGES = 40
-SAVE_EVERY=40
-FILE_NAME="26_02_avito_parser_"
+PAGES = 50
+SAVE_EVERY=50
+FILE_NAME="analitic_avito_dataset_"
 
 def normalize_text(txt:str):
     return txt.replace('\xa0', ' ').replace('\u202f', ' ')
+
+def normilize_time(time:str):
+    time=normalize_text(txt=time)
+    time=time.replace('–',' ')
+    parts=[x for x in time.split(' ')]
+    return parts[1]
+
+
+
+def plus_null(d:str):
+    parts_d=[x.strip() for x in d.split('.')]
+    if len(parts_d[0])<2:
+        parts_d[0]='0'+parts_d[0]
+        return '.'.join(parts_d)
+    return d
+
+def normalize_d(d:str):
+    month={
+        'января': '01',
+        'февраля': '02',
+        'марта': '03',
+        'апреля': '04',
+        'мая': '05',
+        'июня': '06',
+        'июля': '07',
+        'августа': '08',
+        'сентября': '09',
+        'октября': '10',
+        'ноября': '11',
+        'декабря': '12',
+    }
+
+    time={
+        'сегодня': pd.Timestamp.now().strftime('%d.%m.%Y'),
+        'вчера': (pd.Timestamp.now() - pd.Timedelta(days=1)).strftime('%d.%m.%Y'),
+        'сейчас': pd.Timestamp.now().strftime('%d.%m.%Y'),
+    }
+    d=normalize_text(txt=d).lower()
+    parts_d=[x.strip() for x in d.split(' ')]
+    if len(parts_d)<=1:
+        if d in time:
+            return time[d]
+        else:   
+            return d
+    else:
+        index=1
+    
+    for key, value in month.items():
+        if key in parts_d[index]:
+            if ':' in parts_d[2]:
+                d=parts_d[0]+'.'+value+'.'+pd.Timestamp.now().strftime('%Y')
+            else:
+                d=d.replace(key, value)
+            return d.replace(' ', '.')
+        
+    if 'недел' in parts_d[index]:
+        return (pd.Timestamp.now() - pd.Timedelta(days=7*int(parts_d[0]))).strftime('%d.%m.%Y')
+    elif 'месяц' in parts_d[index]:
+        return (pd.Timestamp.now() - pd.Timedelta(days=30*int(parts_d[0]))).strftime('%d.%m.%Y')
+    elif 'год' in parts_d[index]:
+        return (pd.Timestamp.now() - pd.Timedelta(days=365*int(parts_d[0]))).strftime('%d.%m.%Y')
+    elif 'дн' in parts_d[index]:
+        return (pd.Timestamp.now() - pd.Timedelta(days=int(parts_d[0]))).strftime('%d.%m.%Y')
+    elif 'час' in parts_d[index]:
+        return (pd.Timestamp.now() - pd.Timedelta(hours=int(parts_d[0]))).strftime('%d.%m.%Y')
+    elif 'минут' in parts_d[index]:
+        return (pd.Timestamp.now() - pd.Timedelta(minutes=int(parts_d[0]))).strftime('%d.%m.%Y')
+    elif 'секунд' in parts_d[index]:
+        return (pd.Timestamp.now() - pd.Timedelta(seconds=int(parts_d[0]))).strftime('%d.%m.%Y')
+    elif 'лет' in parts_d[index]:
+        return (pd.Timestamp.now() - pd.Timedelta(days=365*int(parts_d[0]))).strftime('%d.%m.%Y')
+    
+    return d
 
 chrome_path = None
 for binary in ['google-chrome', 'google-chrome-stable', 'chromium', 'chromium-browser']:
@@ -29,15 +102,14 @@ if not chrome_path:
     print("Выполни: sudo dnf install chromium")
     exit(1)
 
-# driver = webdriver.Chrome()
 
 options = uc.ChromeOptions()
 options.binary_location = chrome_path
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-blink-features=AutomationControlled")
-# options.add_argument("--disable-setuid-sandbox")
-# options.add_argument("--no-zygote")
-# options.add_argument("--disable-gpu")
+options.add_argument("--disable-setuid-sandbox")
+options.add_argument("--no-zygote")
+options.add_argument("--disable-gpu")
 driver = uc.Chrome(
     options=options,
     use_subprocess=True,
@@ -67,28 +139,39 @@ for page in range(1, PAGES+1):
         title_tag=card.find('a', {'data-marker': 'item-title'})
         title_text=title_tag.get_text(strip=True) if title_tag else ''
         parts=[x.strip() for x in title_text.split(',')]
-        if ' ' in parts[1]:
-            part=[x.strip() for x in parts[1].split(' ')]
-            parts[1]=part[0]
+        if len(parts)>1:
+            parts[1]=normalize_text(txt=parts[1])
+            if len(parts[1])>1:
+                space_part=[x.strip() for x in parts[1].split(' ')]
+                parts[1]=space_part[0]
+
+        if len(parts)<2:
+            continue
 
         price_tag=card.find('span', {'data-marker': 'item-price-value'})
         price=price_tag.get_text(strip=True) if price_tag else None
+        if not price:
+            continue
 
         area_price=None
         for p in card.find_all('p'):
             txt=p.get_text(strip=True)
             txt=normalize_text(txt=txt)
             if txt and ('за м²' in txt or 'за м2' in txt):
-                # print(f"\nfind area_price - {area_price}\n")
-                area_price = txt
-                break
-        
+                area_price = txt.replace('за м²', '').replace('за м2', '').strip()
+        if area_price is None:
+            continue
+            
         street=card.find('a', {'data-marker': 'street_link'})
         house=card.find('a', {'data-marker': 'house_link'})
         address=f"{street.get_text(strip=True) if street else ''}, {house.get_text(strip=True) if house else ''}".strip(', ')
+        if not address:
+            continue
 
         metro_link=card.find('a', {'data-marker': 'metro_link'})
         metro_name=metro_link.get_text(strip=True) if metro_link else ''
+        if not metro_name:
+            continue
 
         time_to_metro=""
         if metro_link:
@@ -96,27 +179,22 @@ for page in range(1, PAGES+1):
             if next_span:
                 time_to_metro=next_span.get_text(strip=True)
                 time_to_metro=time_to_metro.strip(',')
-        # metro=f"{metro_name} {time_to_metro}".strip()
+        time_to_metro=normilize_time(time=time_to_metro)
+        if not time_to_metro:
+            continue
 
-        # description=card.find('p', {'style': '--module-max-lines-size: 4;'})
-        # subtitle=description.get_text(strip=True) if description else ''
-        subtitle=''
-        for p in card.find_all('p'):
-            # subtitle_tag=p.get_attribute_list('style')
-            # if subtitle_tag and (('--module-max-lines-size:\xa04;' in subtitle_tag) or ('--module-max-lines-size:\u202f4;' in subtitle_tag) or ('--module-max-lines-size:\u202f4;' in subtitle_tag)):
-            #     print(f"\nfind subtitle - {p.get_text(strip=True)}\n")
-            #     subtitle = p.get_text(strip=True)
-            #     break
-            style=p.get('style', '')
-            if '--module-max-lines-size' in style and '4' in style:
-                subtitle = p.get_text(strip=True)
-                break
-            
-        date_tag=card.find('div', {'data-marker': 'item-date/wrapper'})
-        date=date_tag.get_text(strip=True) if date_tag else None
+        d_tag=card.find('div', {'data-marker': 'item-date/wrapper'})
+        d=d_tag.get_text(strip=True) if d_tag else None
+        d=normalize_d(d=d) if d else None
+        d=plus_null(d=d) if d else None
+        if not d:
+            continue
 
         link_tag=card.find('a', {'data-marker': 'item-title'})
         source=f"https://www.avito.ru{link_tag.get('href')}" if link_tag else None
+        if not source:
+            continue
+
         data = {
             'type': parts[0] if len(parts) > 0 else None,
             'space': parts[1] if len(parts) > 1 else None,
@@ -125,8 +203,7 @@ for page in range(1, PAGES+1):
             'address': address,
             'metro': metro_name,
             'time_to_metro': time_to_metro,
-            'subtitle': subtitle[:500] + '...' if len(subtitle) > 500 else subtitle,
-            'date': date,
+            'date': d,
             'source': source
         }
         
---
+++ test.py
@@ -0,0 +1,7 @@
+txt='205 628 ₽ за м²'
+if 'за м²' in txt or 'за м2':
+    print(f'205 628 ₽ за м²')
+else: print("No")
+
+print(type(txt))
+print('за м²'=='за м²')
